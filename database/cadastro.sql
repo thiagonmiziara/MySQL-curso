db7f580771d1648366fc13c16a56266048b1c307
thiagonmiziara <thiagonmiziara@gmail.com>
2020-11-22 20:40:53 -0300

    finalizando mais uma aula

--- database/cadastro.sql
+++ database/cadastro.sql
@@ -1,1 +1,6 @@
+/* jeito errado sem definir o conjunto de caracteres
 CREATE DATABASE cadastro;
+*/
+create database cadastro
+default character set utf8
+default collate utf8_general_ci;
--- database/table.sql
+++ database/table.sql
@@ -1,6 +1,18 @@
+/* jeito errado sem a PRIMARY KEY
 CREATE TABLE pessoa(nome VARCHAR (30),
 idade TINYINT ,
 sexo CHAR (1),
 peso FLOAT ,
 altura FLOAT ,
 nacionalidade VARCHAR (20));
+*/
+create table pessoas(
+id int not null auto_increment,
+nome varchar(30) not null,
+nascimento date,
+sexo enum('M','F'),
+peso decimal(5,2),
+altura decimal(3,2),
+nacionalidade varchar(20) default 'Brasil',
+primary key (id)
+)default charset = utf8;
