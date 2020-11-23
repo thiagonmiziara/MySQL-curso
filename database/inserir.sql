d3c6bb75878cf1e8701bfdbb681c2d78c6adfedd
thiagonmiziara <thiagonmiziara@gmail.com>
2020-11-23 15:09:24 -0300

    finalizando mais uma aula

---
+++ database/inserir.sql
@@ -0,0 +1,5 @@
+/* para iserir dados na table usa os comandos INSERT  INTO  */
+insert into pessoas values
+(default,'Brunna','1993-01-15','F','64','1.52',default),
+(default,'Guilherme','1991-12-31','M','66','1.80','Argentina'),
+(default,'Gabriel','1990-07-07','M','64','1.78','Alemanha');
