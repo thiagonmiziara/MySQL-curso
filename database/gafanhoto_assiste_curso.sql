52f276dd78d82a6f4460e8a9c6d1eccc37e44273
thiagonmiziara <thiagonmiziara@gmail.com>
2020-11-26 18:21:10 -0300

    finalizando mais uma aula

---
+++ database/gafanhoto_assiste_curso.sql
@@ -0,0 +1,10 @@
+create table gafanhoto_assiste_curso(
+id int not null auto_increment,
+data date,
+idgafanhoto int,
+idcurso int,
+primary key (id),
+foreign key (idgafanhoto) references gafanhotos(id),
+foreign key (idcurso) references cursos(idcurso)
+
+) default charset = utf8;
